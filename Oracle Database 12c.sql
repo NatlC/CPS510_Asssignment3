6d6476915cdc13ec1d91b83043417494a35b834f
NatlC <104866249+NatlC@users.noreply.github.com>
2024-09-25 01:04:51 -0400

    Add files via upload

---
+++ Oracle Database 12c.sql
@@ -0,0 +1,8 @@
+CREATE TABLE Dentist (
+    DentistID INT PRIMARY KEY,
+    FirstName VARCHAR(50) NOT NULL,
+    LastName VARCHAR(50) NOT NULL,
+    Phone VARCHAR(15) NOT NULL,
+    Email VARCHAR(100) NOT NULL,
+    Specialization VARCHAR(100)
+);
